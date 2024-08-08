ac92e4e124911794e8b9a504763ab35d3262d417
Zygmut <deltas.game@gmail.com>
2024-08-08 18:55:33 +0200

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Zygmut/ducktor
+
+go 1.22.5
---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05") {} }:
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    # https://pre-commit.com/
+    pre-commit
+    golangci-lint
+    gotools
+
+    # https://taskfile.dev/usage/
+    go-task
+
+    go
+  ];
+
+  shellHook = ''
+    alias t=task
+
+    pre-commit install
+  '';
+
+}
